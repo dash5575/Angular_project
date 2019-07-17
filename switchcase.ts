88b7d596fedb255f92de2370bc386fe11b985aea
dash5575 <52546761+dash5575@users.noreply.github.com>
2019-07-18 01:31:52 +0530

    first code

---
+++ switchcase.ts
@@ -0,0 +1,25 @@
+console.log("switch case statement:");
+var Div:string="B";
+
+switch(Div){
+	case "A":
+	{
+console.log("batch A");
+break;
+	}
+	case "B":
+	{
+	console.log("batch B");
+	break;
+	}
+	case "C":
+	{
+	console.log("batch C");
+	break;
+	}
+	default:
+	{
+console.log("invalid batch");
+break;
+	}
+}
